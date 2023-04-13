7241ad9dc7f0a46d0a88f68094058cfa4be1da6c
Maderamel <maderam96@gmail.com>
2023-04-12 19:23:14 -0700

    added more views for differnt analyses

--- EmployeeSQL/Employees.sql
+++ EmployeeSQL/Employees.sql
@@ -101,5 +101,58 @@ employees.emp_no = salaries.emp_no;
 CREATE VIEW hired_86 AS
 SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
-WHERE YEAR(hire_date) = 1986
+WHERE date_part('year', hire_date) = 1986
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name
+CREATE VIEW manager_dept AS
+SELECT dept_manager.dept_no, dept_manager.emp_no, departments.dept_name, employees.last_name, employees.first_name
+FROM dept_manager
+INNER JOIN departments ON
+dept_manager.dept_no = departments.dept_no 
+INNER JOIN employees ON
+dept_manager.emp_no = employees.emp_no
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name 
+CREATE VIEW emp_dept AS
+SELECT dept_emp.dept_no, dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN employees ON
+dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+CREATE VIEW hercules_b AS
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE employees.first_name = 'Hercules' AND
+employees.last_name LIKE 'B%'
+
+--List each employee in the Sales department, including their employee number, last name, and first name
+CREATE VIEW sales_dept_ee AS
+SELECT departments.dept_name, dept_emp.emp_no, employees.last_name, employees.first_name
+FROM employees
+INNER JOIN dept_emp ON
+dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON
+departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales'
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
+CREATE VIEW sales_dev_depts AS
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON
+dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON
+departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales' OR
+departments.dept_name = 'Development'
+
+--List the frequency counts, in descending order, of all the employee last names (counts of employees who share each last name)
+CREATE VIEW last_name_freq AS
+SELECT employees.last_name, count(*)
+FROM employees
+GROUP BY employees.last_name
+ORDER BY count desc
 
